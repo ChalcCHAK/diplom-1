583cd61d46d09c357e8ffcf3acaf66a0f749c500
ChalcCHAK <143936501+ChalcCHAK@users.noreply.github.com>
2024-02-13 17:16:48 +0300

    diplom

--- js/main.js
+++ js/main.js
@@ -1,75 +1,73 @@
 'use strict'
 
-const tabs = document.querySelectorAll('.lishki');
-const tabContent = document.querySelectorAll('.tab');
-const tabParent = document.querySelector('.hero-menu_header');
-
-function hide(){
-    tabContent.forEach(item =>{
-        item.style.display = "none";
-    })
-    tabs.forEach(item =>{
-        item.classList.remove('current');
-    })
-}
-
-function show(i=0){
-    tabContent[i].style.display = "block";
-    tabs[i].classList.add('current');
-}
-
-hide();
-show();
-
-tabParent.addEventListener('click',(event)=>{
-    if(event.target && event.target.classList.contains('lishki')){
-        tabs.forEach((item,i)=>{
-            if(event.target == item){
-                hide();
-                show(i);
-            }
+if(document.querySelector('.lishki')){
+    const tabs = document.querySelectorAll('.lishki');
+    const tabContent = document.querySelectorAll('.tab-content');
+    const tabParent = document.querySelector('.hero-menu_header');
+    
+    function hide(){
+        tabContent.forEach(item =>{
+            item.style.display = "none";
+        })
+        tabs.forEach(item =>{
+            item.classList.remove('current');
         })
     }
-});
-console.log(tabContent);
-
-
-//slidd
-const slides = document.querySelectorAll('.slide-one'),
-prev = document.querySelector('.ec-button-prev'),
-next = document.querySelector('.ec-button-next');
-let slideIndex = 1;
-
-function showSlides(n){
-    if(n>slides.length){
-        slideIndex = 1;
+    
+    function show(i=0){
+        tabContent[i].style.display = "block";
+        tabs[i].classList.add('current');
     }
-    if(n<1){
-        slideIndex = slides.length;
-    }
-    if(slides.length <10){
-        total.textContent = `0${slides.length}`;
-    }else{
-        total.textContent = slides.length;
-    }
-    slides.forEach(item=> item.style.display="none");
-    slides[slideIndex-1].style.display ="block";
-
+    
+    hide();
+    show();
+    
+    tabParent.addEventListener('click',(event)=>{
+        if(event.target && event.target.classList.contains('lishki')){
+            tabs.forEach((item,i)=>{
+                if(event.target == item){
+                    hide();
+                    show(i);
+                }
+            })
+        }
+    });
+    console.log(tabContent);
 }
-showSlides(slideIndex);
 
-function plusSlides(n){
-    showSlides(slideIndex = slideIndex+n);
+
+if(document.querySelector('.slide-one')){
+    const slides = document.querySelectorAll('.slide-one'),
+    prev = document.querySelector('.ec-button-prev'),
+    next = document.querySelector('.ec-button-next');
+    let slideIndex = 1;
+    
+    function showSlides(n){
+        if(n>slides.length){
+            slideIndex = 1;
+        }
+        if(n<1){
+            slideIndex = slides.length;
+        }
+        slides.forEach(item=> item.style.display="none");
+        slides[slideIndex-1].style.display ="block";
+    
+    }
+    showSlides(slideIndex);
+    
+    function plusSlides(n){
+        showSlides(slideIndex = slideIndex+n);
+    }
+    next.addEventListener('click',()=>{
+        plusSlides(1)
+    })
+    prev.addEventListener('click',()=>{
+        plusSlides(-1)
+    })
 }
-next.addEventListener('click',()=>{
-    plusSlides(1)
-})
-prev.addEventListener('click',()=>{
-    plusSlides(-1)
-})
 
-//sliderr
-const slidestwo = document.querySelectorAll('.slide-two'),
+if(document.querySelector('.slide-two')){
+    const slidestwo = document.querySelectorAll('.slide-two'),
 prev2 = document.querySelector('.tc-button-prev'),
 next2 = document.querySelector('.tc-button-next');
 
@@ -82,11 +80,7 @@ function showSlides1(n){
     if(n<1){
         slideIndex2 = slidestwo.length;
     }
-    if(slides.length <10){
-        total.textContent = `0${slidestwo.length}`;
-    }else{
-        total.textContent = slidestwo.length;
-    }
+
     slidestwo.forEach(item=> item.style.display="none");
     slidestwo[slideIndex2-1].style.display="block";
 
@@ -96,12 +90,96 @@ showSlides1(slideIndex2);
 function plusSlides(n){
     showSlides1(slideIndex2 = slideIndex2+n);
 }
-next.addEventListener('click',()=>{
+next2.addEventListener('click',()=>{
     plusSlides(1)
 })
-prev.addEventListener('click',()=>{
+prev2.addEventListener('click',()=>{
     plusSlides(-1)
 })
+
+}
+
+if(document.querySelector('.countdown')){
+    //timer//
+const timeEnd = '2024-02-19';
+
+function getTimeRemaining(endtime){
+    const t = Date.parse(endtime) - Date.parse(new Date()),
+        days = Math.floor(t/(1000*60*60*24)),
+        hours = Math.floor((t/(1000*60*60))%24),
+        minutes = Math.floor((t/(1000*60*60))%60),
+        seconds =  Math.floor((t/(1000))%60);
+    return{
+            'total':t,
+            'days':days,
+            'hours': hours,
+            'minutes':minutes,
+            'seconds':seconds
+        }
+}
+function setClock(selector, endtime){
+    const timer = document.querySelector(selector),
+        days = timer.querySelector('.days'),
+        hours = timer.querySelector('.hours'),
+        minutes = timer.querySelector('.minutes'),
+        seconds = timer.querySelector('.seconds');
+        timeInterval = setInterval(updateClock(),1000);
+    updateClock();
+    function updateClock(){
+        const t = getTimeRemaining(endtime);
+        days.innerHTML = t.days;
+        hours.innerHTML = t.hours;
+        minutes.innerHTML = t.minutes;
+        seconds.innerHTML = t.seconds;
+        if(t.total<=0){
+            clearInterval()
+            days.innerHTML = "00";
+            hours.innerHTML = "00";
+            minutes.innerHTML = "00";
+            seconds.innerHTML = "00";
+        }
+    }
+}
+setClock('.countdown',timeEnd);
+}
+
+
+
+
+if(document.querySelector('.hero-menu_header fl-wrap gth')){
+    const tabs = document.querySelectorAll('.hero-menu_header fl-wrap gth');
+    const tabContent = document.querySelectorAll('.tab-content');
+    const tabParent = document.querySelector('.hero-menu_header');
+    
+    function hide(){
+        tabContent.forEach(item =>{
+            item.style.display = "none";
+        })
+        tabs.forEach(item =>{
+            item.classList.remove('current');
+        })
+    }
+    
+    function show(i=0){
+        tabContent[i].style.display = "block";
+        tabs[i].classList.add('current');
+    }
+    
+    hide();
+    show();
+    
+    tabParent.addEventListener('click',(event)=>{
+        if(event.target && event.target.classList.contains('hero-menu_header fl-wrap gth')){
+            tabs.forEach((item,i)=>{
+                if(event.target == item){
+                    hide();
+                    show(i);
+                }
+            })
+        }
+    });
+    console.log(tabContent);
+}
 
 
 
